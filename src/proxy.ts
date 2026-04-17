3e91d1c16f168ff5362e91aeb653d099a306bbdc
belal987 <belal.m5m8@gmail.com>
2026-04-17 17:01:45 +0200

    fix build

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,10 @@
-export { default as proxy } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export const proxy = withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
 
 export const config = {
   matcher: ["/", "/categories/:path*", "/products/:path*"],
